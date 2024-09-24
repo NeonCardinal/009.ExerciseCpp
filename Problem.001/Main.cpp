c4601da0591ee5aa6de038adb16bae003dbac3f5
NeonCardinal <nvnnaidenov@gmail.com>
2024-09-25 01:09:50 +0300

    Add

---
+++ Problem.001/Main.cpp
@@ -0,0 +1,19 @@
+//Задача 1: Сумиране на две числа
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+
+    cout << a + b << endl;
+
+    return 0;
+}
