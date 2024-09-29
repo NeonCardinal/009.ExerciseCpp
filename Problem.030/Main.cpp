8e076a03621fe1070035e1e6ca608d9bfd5c06fe
NeonCardinal <nvnnaidenov@gmail.com>
2024-09-30 00:45:02 +0300

    Add

---
+++ Problem.030/Main.cpp
@@ -0,0 +1,19 @@
+//Задача 30: Сумиране на последователни числа
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int sum = 0;
+
+    for(int i = 1; i < n; i++)
+    {
+        sum += i;
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
