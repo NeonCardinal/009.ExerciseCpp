5ab34ec8c4b90cb82d18bda7f3a17a2fd791611a
NeonCardinal <nvnnaidenov@gmail.com>
2024-10-01 20:48:50 +0300

    Add

---
+++ Problem.038/Main.cpp
@@ -0,0 +1,20 @@
+//Задача 38: Проверка за делимост с остатък
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+
+    cout << a / b << endl;
+    cout << a % b << endl;
+
+    return 0;
+}
