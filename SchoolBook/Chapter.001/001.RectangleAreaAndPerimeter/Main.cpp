19e86ccf9f06fbd7f790e59a810565d4c47c9a19
NeonCardinal <nvnnaidenov@gmail.com>
2024-10-09 18:18:09 +0300

    Add

---
+++ SchoolBook/Chapter.001/001.RectangleAreaAndPerimeter/Main.cpp
@@ -0,0 +1,17 @@
+//Chapter 1, Task 1
+//08.10.2024, 22:20
+#include <iostream>
+
+int main()
+{
+    double a = 2.3;
+    double b = 3.7;
+
+    double p = 2 * (a + b);
+    double s = a * b;
+
+    std::cout << "Периметъра на правоъгълника е: " << p << std::endl;
+    std::cout << "Лицето на правоъгълника е: " << s << std::endl;
+
+    return 0;
+}
