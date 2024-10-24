409909be9b075197055be4adcd44892a4cd09a61
NeonCardinal <nvnnaidenov@gmail.com>
2024-10-24 20:50:46 +0300

    Add

---
+++ SchoolBook/Chapter.002/Task.001/Main.cpp
@@ -0,0 +1,19 @@
+//Chapter Two, Task 001
+//24.10.2024, 20:38
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+	int y;
+	double x;
+
+	std::cout << "x = ";
+	std::cin >> x;
+
+	y = floor(x);
+
+	std::cout << y << std::endl;
+
+	return 0;
+}
